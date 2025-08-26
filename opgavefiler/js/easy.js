6e9198e8bae445ee4e7e4ccd4210b270840dda64
Marie-Pierre Hélène Lessard <male9@elev.techcollege.dk>
2025-08-26 13:43:19 +0200

    Answers to easy and medium

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,18 +1,48 @@
 /* opgave 1 */
+
+/* The following is not part of the assignment. It groups console logs for each assignment. */
+/* The console.group() static method creates a new inline group in the Web console log, causing any subsequent console messages to be indented by an additional level, until console.groupEnd() is called.
+https://developer.mozilla.org/en-US/docs/Web/API/console/group_static */
 console.group("opgave 1");
 
+/* Trailing commas (sometimes called "final commas") can be useful when adding new elements, parameters, or properties to JavaScript code. If you want to add a new property, you can add a new line without modifying the previously last line if that line already uses a trailing comma. This makes version-control diffs cleaner and editing code might be less troublesome.
+JavaScript has allowed trailing commas in array literals since the beginning. Trailing commas are now also allowed in object literals, function parameters, named imports, named exports, and more.
+JSON, however, disallows all trailing commas. (...)
+JavaScript allows trailing commas wherever a comma-separated list of values is accepted and more values may be expected after the last item. 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Trailing_commas
+*/
+const personByMariePierreLessard = {
+    name: "Jean-Marc",
+    age: 60,
+    job: "foredragsholder"
+};
+console.log(personByMariePierreLessard);
+/* Checked in the console */
+
 console.groupEnd();
 
-/* opgave 2 */
+
+/* opgave 2 (øvelse om udskrivning til konsolen) */
+
+/* The following is not part of the assignment. It groups console logs for each assignment. */
 console.group("opgave 2");
 
-
+console.log(personByMariePierreLessard.name);       // "Jean-Marc"
+console.log(personByMariePierreLessard.job);        // "foredragsholder"
+console.log(`${personByMariePierreLessard.name} arbejder på deltid som ${personByMariePierreLessard.job}.`);
+/* Checked in the console */
 
 console.groupEnd();
+
 
 /* opgave 3 */
+
+/* The following is not part of the assignment. It groups console logs for each assignment. */
 console.group("opgave 3");
-
-
+                
+delete personByMariePierreLessard.age;
+console.log(personByMariePierreLessard);
+/* Checked in the console */
 
 console.groupEnd();
+
--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,22 +1,41 @@
 /* Opgave 1*/
 console.group("opgave 1");
 
+const dataObjectByMariePierreLessard = {
+    itemsArrayByMariePierreLessard: ["opgave 1", "opgave 2", "opgave 3", "opgave 4", "opgave 5"]
+};
 
+// tilføj ny data i en array i et dataobjekt; kig i Array codelab for hint ;)
+/* The push() method is a mutating method. It changes the length and the content of this.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push */
+dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard.push("opgave 6");
+console.log(dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard);
+/* Checked in the console */
 
 console.groupEnd();
-
 
 
 /*opgave 2*/
 console.group("opgave 2");
 
-
+// fjern item 2 fra arrayen i dataobjektet; kig i Array codelab for hint ;)
+/* Note: splice removes items from the index specified. Here, the starting point is the first one, and the number of items removed from that point is 1. */
+/* The splice() method is a mutating method. It may change the content of this.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice */
+dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard.splice(1, 1);
+console.log(dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard);
+/* Checked in the console */
 
 console.groupEnd();
 
 
 /*opgave 3*/
 console.group("opgave 3");
+
+/* Since splice is a mutating method, this no longer has "opgave 2" at index 1. Below, "opgave 3" is getting replaced by "changed" because it is now at index 1. */
+dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard.splice(1, 1, "changed");
+console.log(dataObjectByMariePierreLessard.itemsArrayByMariePierreLessard);
+/* Checked in the console */
 
 console.groupEnd();
 
