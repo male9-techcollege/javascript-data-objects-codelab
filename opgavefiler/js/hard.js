dba3003d294d6a9cdf075057fe9c111f6a63b786
Marie-Pierre Hélène Lessard <male9@elev.techcollege.dk>
2025-08-26 14:48:21 +0200

    Hard exercises done

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -3,7 +3,7 @@ console.group("opgave 1");
 
 /* Several things are nested in the following constant.
 The data object toDoAppObjectByMariePierreLessard contains 2 objects (curly brackets): errandsByMariePierreLessard and homeworkByMariePierreLessard. Each object is meant to be a list with a name/title and elements ("opgaver"/tasks) in the list.
-The tasks go in an array (square brackets) inside of each nested object.
+The tasks go in an array (square brackets) inside of each nested object, which contains key-value pairs.
 */
 const toDoAppObjectByMariePierreLessard = {
     errandsByMariePierreLessard: {
@@ -35,24 +35,44 @@ console.groupEnd();
 /* opgave 2 */
 console.group("opgave 2");
 
-/* This assignment is not finished */
+/* The values of listKey and itemText become known when the function addItemByMariePierreLessard is called. */
 function addItemByMariePierreLessard(listKey, itemText) {
-    const list = todoApp[listKey];
-    if (list) {
-        const newItem = {text: itemText, done: false };
-        list.items.push(newItem);
+    const listOfToDosByMariePierreLessard = toDoAppObjectByMariePierreLessard[listKey];
+    if (listOfToDosByMariePierreLessard) {
+        /* I.e. if that list exists */
+        const newItemByMariePierreLessard = {text: itemText, done: false };
+        /* The new task gets added to the array after the key items. */
+        listOfToDosByMariePierreLessard.items.push(newItemByMariePierreLessard);
     };
-    console.log(todoApp);
-}
+    console.log(toDoAppObjectByMariePierreLessard);
+};
 
+/* The function above is called. */
 addItemByMariePierreLessard("errandsByMariePierreLessard", "Vegetable oil");
-
-                
+/* Checked in the console */
 
 console.groupEnd();
 
 
 /* opgave 3 */
 console.group("opgave 3");
+
+/* The values of listKey and itemNr become known when the function toggleItemDoneByMariePierreLessard is called. */
+function toggleItemDoneByMariePierreLessard(listKey, itemNr) {
+    const listOfToDosByMariePierreLessard = toDoAppObjectByMariePierreLessard[listKey];
+    if (listOfToDosByMariePierreLessard) {
+        /* I.e. if that list exists */
+        const itemByMariePierreLessard = listOfToDosByMariePierreLessard.items[itemNr];
+        if (itemByMariePierreLessard) {
+        /* I.e. if that item exists */
+                    itemByMariePierreLessard.done = !itemByMariePierreLessard.done;
+                    /* I.e. if the boolean value of done was true, it becomes false thanks to the above, and vice versa. */
+        };
+    };
+    console.log(toDoAppObjectByMariePierreLessard);
+};
+
+toggleItemDoneByMariePierreLessard("homeworkByMariePierreLessard", 1);
+/* Checked in the console */
 
 console.groupEnd();
